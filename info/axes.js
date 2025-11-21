5d0777c985fc9453d95ba516a88f0b04605fbece
unknown <heyujia@aliyun.com>
2025-11-21 21:49:11 +0800

    set up a imei test website

---
+++ info/axes.js
@@ -0,0 +1,66 @@
+axes = [
+    {
+        "id": "imei",
+        "name": "独尊儒术",
+        "left": "反孔精英",
+        "right": "忠心耿耿",
+        "leftEmoji": "🐕",
+        "rightEmoji": "👑"
+    },
+    {
+        "id": "basic",
+        "name": "王之常识",
+        "left": "地上足球",
+        "right": "懂梅西帝",
+        "leftEmoji": "💊",
+        "rightEmoji": "✊"
+    },
+    {
+        "id": "agf",
+        "name": "阿根廷娜",
+        "left": "水果海盗",
+        "right": "精阿狂喜",
+        "leftEmoji": "🛌",
+        "rightEmoji": "👨‍💻"
+    },
+    {
+        "id": "fcb",
+        "name": "巴塞罗那",
+        "left": "皇马间谍",
+        "right": "铁血萨米",
+        "leftEmoji": "🔪",
+        "rightEmoji": "🐺"
+    },
+    {
+        "id": "hk",
+        "name": "香港球王",
+        "left": "小头一低",
+        "right": "合同之王",
+        "leftEmoji": "🕊️",
+        "rightEmoji": "👁️"
+    },
+    {
+        "id": "goat",
+        "name": "金球封王",
+        "left": "绝世神偷",
+        "right": "全体起立",
+        "leftEmoji": "🗽",
+        "rightEmoji": "⚽"
+    },
+    {
+        "id": "world",
+        "name": "大力神杯",
+        "left": "汰伪神杯",
+        "right": "天神下凡",
+        "leftEmoji": "💰",
+        "rightEmoji": "⚙️"
+    },
+    {
+        "id": "america",
+        "name": "美洲捧杯",
+        "left": "五四运动",
+        "right": "哥哥威武",
+        "leftEmoji": "🌐",
+        "rightEmoji": "🚧"
+    }
+]; 
